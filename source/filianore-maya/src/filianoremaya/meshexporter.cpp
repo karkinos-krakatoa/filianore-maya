278982fac639fdc638ddf1c648a9b6b9dca038de
Karkinos Krakatoa <femloh@gmail.com>
2021-01-29 12:22:17 +0000

    added uv getter

--- source/filianore-maya/src/filianoremaya/meshexporter.cpp
+++ source/filianore-maya/src/filianoremaya/meshexporter.cpp
@@ -1,9 +1,11 @@
 #include <maya/MItDag.h>
 #include <maya/MDagPath.h>
+#include <maya/MFloatArray.h>
 #include <maya/MFloatVectorArray.h>
 #include <maya/MIntArray.h>
 #include <maya/MMatrix.h>
 #include <maya/MPointArray.h>
+#include <maya/MPoint.h>
 #include <maya/MGlobal.h>
 
 #include "util.h"
@@ -22,6 +24,7 @@
 using namespace filianore;
 
 using Vec3 = StaticArray<float, 3>;
+using Vec2 = StaticArray<float, 2>;
 using TriEntity = TriangleEntity;
 
 MayaMesh::MayaMesh(MFnMesh &_mesh)
@@ -74,24 +77,43 @@ MayaMesh::MayaMesh(MFnMesh &_mesh)
 
         for (int i = 0; i < mTrianglesCount[polygon]; ++i)
         {
+            MStatus uvGetterStatus;
+
+            Vec2 uv1(0.f, 0.f);
+            Vec2 uv2(1.f, 0.f);
+            Vec2 uv3(1.f, 1.f);
+            float2 uv;
+            MPoint pointForUV = MPoint(0.f, 0.f, 0.f);
+
             int vertexIds[3];
             _mesh.getPolygonTriangleVertices(polygon, i, vertexIds);
 
             Vec3 triV1 = Vec3(vertices[vertexIds[0]]);
+            pointForUV = MPoint(triV1.x(), triV1.y(), triV1.z());
+            _mesh.getUVAtPoint(pointForUV, uv, MSpace::kWorld);
+            uv1 = Vec2(uv[0], uv[1]);
+
             Vec3 triV2 = Vec3(vertices[vertexIds[1]]);
+            pointForUV = MPoint(triV2.x(), triV2.y(), triV2.z());
+            _mesh.getUVAtPoint(pointForUV, uv, MSpace::kWorld);
+            uv2 = Vec2(uv[0], uv[1]);
+
             Vec3 triV3 = Vec3(vertices[vertexIds[2]]);
+            pointForUV = MPoint(triV3.x(), triV3.y(), triV3.z());
+            _mesh.getUVAtPoint(pointForUV, uv, MSpace::kWorld);
+            uv3 = Vec2(uv[0], uv[1]);
 
             Vec3 triN1 = Vec3(normals[verticesToNormals[vertexIds[0]]]);
             Vec3 triN2 = Vec3(normals[verticesToNormals[vertexIds[1]]]);
             Vec3 triN3 = Vec3(normals[verticesToNormals[vertexIds[2]]]);
 
-            TriEntity t1(triV1, triN1, true);
-            TriEntity t2(triV2, triN2, true);
-            TriEntity t3(triV3, triN3, true);
+            TriEntity t1(triV1, triN1, true, uv1);
+            TriEntity t2(triV2, triN2, true, uv2);
+            TriEntity t3(triV3, triN3, true, uv3);
 
             std::shared_ptr<Shape> triangle = std::make_shared<Triangle>(t1, t2, t3);
 
-            RGBSpectrum col(0.f, 0.277f, 1.f);
+            RGBSpectrum col(0.5f);
             std::shared_ptr<Texture<RGBSpectrum>> tex = std::make_shared<ConstantTexture<RGBSpectrum>>(col);
             std::shared_ptr<Material> material = std::make_shared<LambertMaterial>(tex);
 
