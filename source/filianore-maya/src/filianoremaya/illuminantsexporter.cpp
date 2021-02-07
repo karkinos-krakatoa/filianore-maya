6addf890a811ce2f8bd53261481e62d209a097bc
Karkinos Krakatoa <femloh@gmail.com>
2021-02-07 02:25:35 +0000

    Added SpotLights

--- source/filianore-maya/src/filianoremaya/illuminantsexporter.cpp
+++ source/filianore-maya/src/filianoremaya/illuminantsexporter.cpp
@@ -9,6 +9,7 @@
 #include <maya/MGlobal.h>
 #include <maya/MFnLight.h>
 #include <maya/MFnPointLight.h>
+#include <maya/MFnSpotLight.h>
 #include <maya/MColor.h>
 
 #include "illuminantsexporter.h"
@@ -18,6 +19,7 @@
 #include "filianore/color/rgb.h"
 
 #include "filianore/illuminants/point.h"
+#include "filianore/illuminants/spot.h"
 
 using namespace filianore;
 
@@ -37,6 +39,10 @@ std::vector<std::shared_ptr<Illuminant>> IlluminantExporter::ExportIlluminants()
             continue;
         }
 
+        TransformVectors transformData = GetDagObjectTransformData(dagPath);
+        Transform transform = Translate(transformData.Translate);
+        //transform = transform * (RotateX(transformData.Rotate.x(), true) * RotateY(transformData.Rotate.y(), true) * RotateZ(transformData.Rotate.z(), true));
+
         if (dagPath.hasFn(MFn::kPointLight))
         {
             MFnPointLight pointIllum(dagPath, &status);
@@ -46,13 +52,33 @@ std::vector<std::shared_ptr<Illuminant>> IlluminantExporter::ExportIlluminants()
                 continue;
             }
 
-            TransformVectors transformData = GetDagObjectTransformData(dagPath);
             RGBSpectrum color = RGBSpectrum(pointIllum.color().r, pointIllum.color().g, pointIllum.color().b);
             RGBSpectrum shadowColor = RGBSpectrum(pointIllum.shadowColor().r, pointIllum.shadowColor().g, pointIllum.shadowColor().b);
             float intensity = pointIllum.intensity();
 
-            std::shared_ptr<Illuminant> actualPointIllum = std::make_shared<PointIlluminant>(transformData.Translate, color, intensity, pointIllum.decayRate(), shadowColor);
-            illums.emplace_back(actualPointIllum);
+            std::shared_ptr<Illuminant> actualIllum = std::make_shared<PointIlluminant>(transform, color, intensity, pointIllum.decayRate(), shadowColor);
+            illums.emplace_back(actualIllum);
+        }
+
+        if (dagPath.hasFn(MFn::kSpotLight))
+        {
+            MFnSpotLight spotLight(dagPath, &status);
+            if (!status)
+            {
+                status.perror("MFnLight constructor");
+                continue;
+            }
+
+            RGBSpectrum color = RGBSpectrum(spotLight.color().r, spotLight.color().g, spotLight.color().b);
+            RGBSpectrum shadowColor = RGBSpectrum(spotLight.shadowColor().r, spotLight.shadowColor().g, spotLight.shadowColor().b);
+            float intensity = spotLight.intensity();
+            MFloatVector direction = spotLight.lightDirection(0, MSpace::kWorld);
+
+            std::shared_ptr<Illuminant> actualIllum = std::make_shared<SpotIlluminant>(transform, StaticArray<float, 3>(direction.x, direction.y, direction.z),
+                                                                                       (float)spotLight.coneAngle(), (float)spotLight.penumbraAngle(),
+                                                                                       true, true,
+                                                                                       color, intensity, spotLight.decayRate(), shadowColor);
+            illums.emplace_back(actualIllum);
         }
     }
 
